9a160f063e34cd7fbddd6398ca3aa9d8f5bdbe90
Marcelo Torres <marcelo-torres>
2019-06-07 13:32:14 -0300

    funcao para remover pontuacao

--- codigo/normalizador.py
+++ codigo/normalizador.py
@@ -8,6 +8,7 @@ class Normalizador:
         with open(caminho_email, "r+") as arquivo_email:
             conteudo_email = arquivo_email.read()
 
+            conteudo_email = self.remover_pontuacao(conteudo_email)
             conteudo_email = self.remover_cabecalho(conteudo_email)
             conteudo_email = self.substituir_caracteres_acentuados(conteudo_email)
             conteudo_email = self.converter_caracteres_para_minusculo(conteudo_email)
@@ -17,10 +18,17 @@ class Normalizador:
             arquivo_email.truncate()
         arquivo_email.close()
 
+    # TODO
+    def remover_pontuacao(self, conteudo_email):
+        novo_conteudo = conteudo_email
+        return novo_conteudo
+
+    # TODO
     def remover_cabecalho(self, conteudo_email):
         novo_conteudo = conteudo_email
         return novo_conteudo
 
+    # TODO
     def substituir_caracteres_acentuados(self, conteudo_email):
         novo_conteudo = conteudo_email
         return novo_conteudo
