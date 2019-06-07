09b9963ace97fa6f2d0c164fa2bc5d737daf8719
Marcelo Torres <marcelo-torres>
2019-06-07 13:14:53 -0300

    teste de normalizacao

--- codigo/main.py
+++ codigo/main.py
@@ -1,1 +1,10 @@
+from codigo.normalizador import Normalizador
+
 print('olar')
+
+def main():
+    normalizador = Normalizador()
+    normalizador.normalizar_email("../dados/0_todos_os_emails/1.txt")
+
+if __name__ == '__main__':
+    main()
